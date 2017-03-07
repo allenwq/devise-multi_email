da9ab452598066c5ee5d28a7dc442db5ceac290c
WANG QIANG <rovingbreeze@gmail.com>
2017-03-07 23:06:04 +0800

    Example app: Add devise encryptable

--- examples/rails5_app/app/models/user.rb
+++ examples/rails5_app/app/models/user.rb
@@ -3,5 +3,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :lockable, :timeoutable and :omniauthable
   devise :multi_email_authenticatable, :multi_email_confirmable, :multi_email_validatable,
-         :recoverable, :registerable, :rememberable, :trackable
+         :recoverable, :registerable, :rememberable, :trackable,
+         # Below are for testing purpose, not required by default
+         :encryptable, encryptor: :sha512
 end
--- examples/rails5_app/db/schema.rb
+++ examples/rails5_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170307140813) do
+ActiveRecord::Schema.define(version: 20170307145547) do
 
   create_table "emails", force: :cascade do |t|
     t.integer  "user_id",                              null: false
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20170307140813) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "password_salt"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 

d4e68e008f8b42238467ef8c60ed292302782409
Joel Van Horn <joel@joelvanhorn.com>
2017-05-12 15:26:46 -0400

    Abstract out helper methods to handle customizations

--- lib/devise/multi_email.rb
+++ lib/devise/multi_email.rb
@@ -23,6 +23,83 @@ module Devise
     def self.emails_association_name=(name)
       @emails_association_name = name.try(:to_sym)
     end
+
+    module ParentModelExtensions
+      extend ActiveSupport::Concern
+
+      included do
+        _emails_association_class.send :include, EmailModelExtensions
+      end
+
+      # Gets the primary email record.
+      def primary_email_record
+        valid_emails = _emails_association.each.select do |email_record|
+          !email_record.destroyed? && !email_record.marked_for_destruction?
+        end
+
+        result = valid_emails.find(&:primary?)
+        result ||= valid_emails.first
+        result
+      end
+
+      def _emails_association
+        __send__(self.class._emails_association_name)
+      end
+
+      module ClassMethods
+
+        def _emails_association_class
+          unless _reflect_on_emails_association
+            raise "#{self}##{Devise::MultiEmail.emails_association_name} association not found: It might be because your declaration is after `devise :multi_email_confirmable`."
+          end
+
+          @_emails_association_class ||= _reflect_on_emails_association.class_name.constantize
+        end
+
+        def _emails_association_table_name
+          @_emails_association_table_name ||= _reflect_on_emails_association.table_name
+        end
+
+        def _reflect_on_emails_association
+          @_reflect_on_emails_association ||= reflect_on_association(_emails_association_name)
+        end
+
+        def _emails_association_name
+          Devise::MultiEmail.emails_association_name
+        end
+      end
+    end
+
+    module EmailModelExtensions
+      extend ActiveSupport::Concern
+
+      def _parent_association
+        __send__(self.class._parent_association_name)
+      end
+
+      module ClassMethods
+
+        def _parent_association_class
+          unless _reflect_on_parent_association
+            raise "#{self}##{Devise::MultiEmail.parent_association_name} association not found: It might be because your declaration is after `devise :multi_email_confirmable`."
+          end
+
+          @_parent_association_class ||= _reflect_on_parent_association.class_name.constantize
+        end
+
+        def _parent_association_table_name
+          @_parent_association_table_name ||= _reflect_on_parent_association.table_name
+        end
+
+        def _reflect_on_parent_association
+          @_reflect_on_parent_association ||= reflect_on_association(_parent_association_name)
+        end
+
+        def _parent_association_name
+          Devise::MultiEmail.parent_association_name
+        end
+      end
+    end
   end
 end
 

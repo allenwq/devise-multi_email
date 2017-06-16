ab2af1f48ed33515995c228bb0edffb783024767
Joel Van Horn <joel@joelvanhorn.com>
2017-06-16 01:07:09 +0100

    clean up class methods

--- lib/devise/multi_email.rb
+++ lib/devise/multi_email.rb
@@ -3,52 +3,54 @@ require 'devise'
 
 module Devise
   module MultiEmail
-    def self.configure(&block)
-      yield self
-    end
+    class << self
+      def configure(&block)
+        yield self
+      end
 
-    @autosave_emails = false
+      @autosave_emails = false
 
-    def self.autosave_emails?
-      @autosave_emails == true
-    end
+      def autosave_emails?
+        @autosave_emails == true
+      end
 
-    def self.autosave_emails=(value)
-      @autosave_emails = (value == true)
-    end
+      def autosave_emails=(value)
+        @autosave_emails = (value == true)
+      end
 
-    @only_login_with_primary_email = false
+      @only_login_with_primary_email = false
 
-    def self.only_login_with_primary_email?
-      @only_login_with_primary_email == true
-    end
+      def only_login_with_primary_email?
+        @only_login_with_primary_email == true
+      end
 
-    def self.only_login_with_primary_email=(value)
-      @only_login_with_primary_email = (value == true)
-    end
+      def only_login_with_primary_email=(value)
+        @only_login_with_primary_email = (value == true)
+      end
 
-    def self.parent_association_name
-      @parent_association_name ||= :user
-    end
+      def parent_association_name
+        @parent_association_name ||= :user
+      end
 
-    def self.parent_association_name=(name)
-      @parent_association_name = name.try(:to_sym)
-    end
+      def parent_association_name=(name)
+        @parent_association_name = name.try(:to_sym)
+      end
 
-    def self.emails_association_name
-      @emails_association_name ||= :emails
-    end
+      def emails_association_name
+        @emails_association_name ||= :emails
+      end
 
-    def self.emails_association_name=(name)
-      @emails_association_name = name.try(:to_sym)
-    end
+      def emails_association_name=(name)
+        @emails_association_name = name.try(:to_sym)
+      end
 
-    def self.primary_email_method_name
-      @primary_email_method_name ||= :primary_email_record
-    end
+      def primary_email_method_name
+        @primary_email_method_name ||= :primary_email_record
+      end
 
-    def self.primary_email_method_name=(name)
-      @primary_email_method_name = name.try(:to_sym)
+      def primary_email_method_name=(name)
+        @primary_email_method_name = name.try(:to_sym)
+      end
     end
   end
 end
